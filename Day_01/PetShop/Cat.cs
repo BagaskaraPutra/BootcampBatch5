fac3155d72e2c759ac4dc6e91f5696a842e9d110
BagaskaraPutra <primastyaputra@gmail.com>
2023-09-04 12:09:41 +0700

    first commit

---
+++ Day_01/PetShop/Cat.cs
@@ -0,0 +1,20 @@
+namespace HewanDarat;
+
+public class Cat
+{
+	public string name;
+	public string gender;
+	public bool isStripe;
+	
+	public void Meow()
+	
+	{
+		Console.WriteLine("Meow");
+	}
+	
+	public void Jump()
+	
+	{
+		Console.WriteLine("Jump");
+	}
+}
---
+++ Day_01/PetShop/Program.cs
@@ -0,0 +1,15 @@
+﻿using HewanDarat;
+class Program
+
+{
+	static  void Main()
+	
+	{
+		Cat burhan = new Cat();
+		Cat cibi = new Cat();
+		burhan.name = "Burhan";
+		cibi.name = "Cibi";
+		Console.WriteLine(burhan.name);
+		Console.WriteLine(cibi.name);
+	}
+}
