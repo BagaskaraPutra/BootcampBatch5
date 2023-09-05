1cd39cc1d98c56a5a208fd394684fdfd7206778a
BagaskaraPutra <primastyaputra@gmail.com>
2023-09-05 12:08:27 +0700

    Add Day_02 solution

---
+++ Day_02/Auto/Tire.cs
@@ -0,0 +1,13 @@
+namespace Auto;
+
+public class Tire
+{
+	private string _brandName;
+	private int _radius;
+	
+	public Tire(string brandName, int radius)
+	{
+		this._brandName = brandName;
+		this._radius = radius;
+	}
+}
