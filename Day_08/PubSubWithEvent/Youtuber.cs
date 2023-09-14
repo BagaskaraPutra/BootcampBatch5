3e3a8ed980eb5b9c485a75055809751dc0043f25
BagaskaraPutra <primastyaputra@gmail.com>
2023-09-14 08:10:33 +0700

    Add static ID to count number of videos on Youtube

--- Day_08/PubSubWithEvent/Youtuber.cs
+++ Day_08/PubSubWithEvent/Youtuber.cs
@@ -11,6 +11,7 @@ public class Youtuber
 {
 	private EventHandler<EventData> _subscriber; // generic type EventHandler
 	private string _name;
+	private static int _id;
 	
 	public Youtuber(string name)
 	{
@@ -19,95 +20,13 @@ public class Youtuber
 	public void UploadVideo()
 	{
 		Console.WriteLine($"{this._name} is uploading a video...");
+		_id++;
 		SendNotification();
 	}
 	public void SendNotification() {
 		if(_subscriber != null) 
 		{
-			// Console.WriteLine($"{this._name} is sending notification to subscribers ...");
-			_subscriber.Invoke(this, new EventData { id = 1, message = "Uploaded Video" });
-		}
-	}
-	public bool AddSubscriber(EventHandler<EventData> sub) {
-		if(_subscriber is null || !_subscriber.GetInvocationList().Contains(sub))
-		{
-		 	_subscriber += sub;
-			return true;
-		}
-		return false;
-	}
-	public void RemoveSubscriber(EventHandler<EventData> sub) {
-		_subscriber -= sub;
-	}
-	public void EventClear() {
-		_subscriber = null;
-	}
-	public override string ToString()
-	{
-		 return _name;
-	}
-}
-
-public class OldGenYoutuber
-{
-	private EventHandler _subscriber;
-	private string _name;
-	
-	public OldGenYoutuber(string name)
-	{
-		_name = name;
-	}
-	public void UploadVideo()
-	{
-		Console.WriteLine($"{this._name} is uploading a video...");
-		SendNotification();
-	}
-	public void SendNotification() {
-		if(_subscriber != null) 
-		{
-			// Console.WriteLine($"{this._name} is sending notification to subscribers ...");
-			_subscriber.Invoke(this, new EventData { id = 1, message = "Uploaded Video" });
-		}
-	}
-	public bool AddSubscriber(EventHandler sub) {
-		if(_subscriber is null || !_subscriber.GetInvocationList().Contains(sub))
-		{
-		 	_subscriber += sub;
-			return true;
-		}
-		return false;
-	}
-	public void RemoveSubscriber(EventHandler sub) {
-		_subscriber -= sub;
-	}
-	public void EventClear() {
-		_subscriber = null;
-	}
-	public override string ToString()
-	{
-		 return _name;
-	}
-}
-
-public class NewGenYoutuber
-{
-	private EventHandler<EventData> _subscriber; // generic type EventHandler
-	private string _name;
-	
-	public NewGenYoutuber(string name)
-	{
-		_name = name;
-	}
-	public void UploadVideo()
-	{
-		Console.WriteLine($"{this._name} is uploading a video...");
-		SendNotification();
-	}
-	public void SendNotification() {
-		if(_subscriber != null) 
-		{
-			// Console.WriteLine($"{this._name} is sending notification to subscribers ...");
-			_subscriber.Invoke(this, new EventData { id = 1, message = "Uploaded Video" });
+			_subscriber.Invoke(this, new EventData { id = _id, message = "Uploaded Video" });
 		}
 	}
 	public bool AddSubscriber(EventHandler<EventData> sub) {
