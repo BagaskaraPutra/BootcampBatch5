aba0d03dee3fdcbdcaa16ae5ce0cef5b5ee48445
BagaskaraPutra <primastyaputra@gmail.com>
2023-09-14 10:34:39 +0700

    Convert delegate to equivalent Func

--- Day_09/PeopleDelegate/Program.cs
+++ Day_09/PeopleDelegate/Program.cs
@@ -12,11 +12,12 @@ public class Person
 class Program
 {
 	//Our delegate
-	public delegate bool FilterDelegate(Person p);
-
+	// public delegate bool FilterDelegate(Person p); // original
+	
 	static void Main(string[] args)
 	{
-
+		Func<Person,bool> FilterDelegate; // added
+		
 		//Create 4 Person objects
 		Person p1 = new Person() { Name = "John", Age = 41 };
 		Person p2 = new Person() { Name = "Jane", Age = 69 };
@@ -25,7 +26,6 @@ class Program
 
 		//Create a list of Person objects and fill it
 		List<Person> people = new List<Person>() { p1, p2, p3, p4 };
-
 		//Invoke DisplayPeople using appropriate delegate
 		DisplayPeople("Children:", people, IsChild);
 		DisplayPeople("Adults:", people, IsAdult);
@@ -40,8 +40,11 @@ class Program
 	/// <param name="people">A list of people</param>
 	/// <param name="filter">A filter</param>
 	/// <returns>A filtered list</returns>
-	static void DisplayPeople(string title, List<Person> people, FilterDelegate filter)
+	
+	// static void DisplayPeople(string title, List<Person> people, FilterDelegate filter)
+	static void DisplayPeople(string title, List<Person> people, Func<Person,bool> filter)
 	{
+		
 		Console.WriteLine(title);
 
 		foreach (Person p in people)
