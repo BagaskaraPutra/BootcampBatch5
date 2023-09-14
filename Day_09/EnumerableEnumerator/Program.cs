d45bb2bce98a1393a352cdcd43c3f3fcdd1e2512
BagaskaraPutra <primastyaputra@gmail.com>
2023-09-14 10:16:24 +0700

    Add Enumerable Enumerator examples

---
+++ Day_09/EnumerableEnumerator/Program.cs
@@ -0,0 +1,43 @@
+﻿public class Program
+{
+	static void Main()
+	{
+		List<int> x = new List<int>() { 1, 2, 3 };
+
+		Console.WriteLine("Iterating using foreach: ");
+		foreach (var i in x)
+		{
+			Console.WriteLine(i);
+		}
+
+		Console.WriteLine("Iterating using Enumerator: ");
+		IEnumerator<int> iterator = x.GetEnumerator();
+		while (iterator.MoveNext())
+		{
+			Console.WriteLine(iterator.Current);
+		}
+
+		int currentInt = iterator.Current;
+		Console.WriteLine("Manual Enumerator: ");
+		iterator.Reset();
+		currentInt = iterator.Current;
+		Console.WriteLine(currentInt);
+		iterator.MoveNext();
+		currentInt = iterator.Current;
+		Console.WriteLine(currentInt);
+		iterator.MoveNext();
+		currentInt = iterator.Current;
+		Console.WriteLine(currentInt);
+		iterator.MoveNext();
+		currentInt = iterator.Current;
+		Console.WriteLine(currentInt);
+		iterator.MoveNext();
+		currentInt = iterator.Current;
+		Console.WriteLine(currentInt);
+		iterator.MoveNext();
+		currentInt = iterator.Current;
+		Console.WriteLine(currentInt);
+		iterator.MoveNext();
+		currentInt = iterator.Current;
+	}
+}
