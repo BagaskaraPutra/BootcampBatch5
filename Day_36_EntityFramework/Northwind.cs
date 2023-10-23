562c849b9f2ba57a5c71ba57abf97979897ca168
BagaskaraPutra <primastyaputra@gmail.com>
2023-10-23 10:12:22 +0700

    Add Day_36 solution

---
+++ Day_36_EntityFramework/Northwind.cs
@@ -0,0 +1,20 @@
+
+using System.ComponentModel.DataAnnotations;
+using EFCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFDatabase;
+
+public class Northwind : DbContext
+{
+	public DbSet<Category>? categories;
+	public DbSet<Product>? product;
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+	{
+		string path = Path.Combine( Environment.CurrentDirectory, "Northwind.db");
+		// string connection = ($"Data Source={path}");
+		string connection = ($"Filename={path}");
+		Console.WriteLine($"Connection: {connection}");
+		optionsBuilder.UseSqlite(connection);
+	}
+}
---
+++ Day_36_EntityFramework/Program.cs
@@ -0,0 +1,14 @@
+﻿using EFDatabase;
+
+class MainProgram
+{
+	static void Main() 
+	{
+		using(Northwind db = new Northwind()) 
+		{
+			Console.WriteLine($"Name: {db.Database}");
+			Console.WriteLine($"Database.ProviderName: {db.Database.ProviderName}");
+			Console.WriteLine($"Database.CanConnect(): {db.Database.CanConnect()}");
+		}
+	}
+}
